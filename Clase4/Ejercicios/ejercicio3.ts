f220ed1b3781be930f04232dff21860874a3cbae
bernibauque <bauquebernardita@gmail.com>
2025-04-08 18:20:31 -0300

    clase 4

---
+++ Clase4/Ejercicios/ejercicio3.ts
@@ -0,0 +1,8 @@
+function identidad<T>(valor: T): T {
+    return valor;
+  }
+  
+  // Llamadas de ejemplo:
+  console.log(identidad<number>(5));    // 5
+  console.log(identidad<string>("Hola")); // "Hola"
+  
